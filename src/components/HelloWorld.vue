7b5f53da84dd3e7ec0e6ad64f47487a17b8af376
napoleon-commits <62509875+napoleon-commits@users.noreply.github.com>
2021-01-20 15:28:45 -0600

    Initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-container>
+    <KingAndPawn />
+  </v-container>
+</template>
+
+<script>
+  import KingAndPawn from '@/components/subcomponents/KingAndPawn';
+
+  export default {
+    name: 'HelloWorld',
+    components: {
+      KingAndPawn,
+    },
+  }
+</script>
---
+++ src/components/subcomponents/KingAndPawn.vue
@@ -0,0 +1,38 @@
+<template>
+    <v-dialog>
+        <template v-slot:activator="{ on, attrs }">
+            <v-btn
+                v-bind="attrs"
+                v-on="on"
+                block
+                outlined
+            >
+                <span>
+                    The magic position
+                </span>
+            </v-btn>
+        </template>
+        <v-list>
+            <v-list-item>
+                <v-list-item-title class="text-wrap">
+                    <CustomDialog />
+                </v-list-item-title>
+            </v-list-item>
+        </v-list>
+    </v-dialog>
+</template>
+
+<script>
+import CustomDialog from '@/components/subcomponents/CustomDialog';
+
+export default {
+    name: 'KingAndPawn',
+    components: {
+        CustomDialog,
+    },
+}
+</script>
+
+<style>
+
+</style>
