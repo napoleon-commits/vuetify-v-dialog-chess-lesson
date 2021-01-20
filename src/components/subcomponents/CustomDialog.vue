56cc49e98607284b5545124d7f34ff653b05d44c
Byron Hall <byronhall@MacBook-Pro.attlocal.net>
2021-01-20 16:42:08 -0600

    added pagination component

--- src/components/subcomponents/CustomDialog.vue
+++ src/components/subcomponents/CustomDialog.vue
@@ -1,7 +1,13 @@
 <template>
     <v-row>
         <v-col>
-            {{demo[page-1].text}}
+            <div>{{demo[page-1].text}}</div>
+            <div>
+                <v-pagination
+                    v-model="page"
+                    :length="demo.length"
+                ></v-pagination>
+            </div>
         </v-col>
         <v-col>
             Integer porta eros orci, porttitor dignissim tellus semper non. Integer lobortis massa eu convallis imperdiet. Etiam et orci vitae orci viverra porttitor sit amet ac nisl. Aliquam rhoncus vitae arcu id sollicitudin. Sed pharetra iaculis velit eget viverra. Donec euismod, nisi et laoreet pharetra, dui erat sollicitudin massa, viverra dictum nunc turpis eu libero. Duis ut nibh massa. Nullam fermentum felis at nunc mollis gravida. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eget ipsum nec enim gravida volutpat. Integer vitae convallis mi. Morbi mollis ullamcorper ullamcorper. Donec vel tristique arcu, eu laoreet nunc. Donec efficitur rhoncus luctus. Proin at feugiat velit, vitae hendrerit odio.
