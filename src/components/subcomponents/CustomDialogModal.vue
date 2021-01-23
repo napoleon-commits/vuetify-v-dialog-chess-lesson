c4f89492effd30e711fff9fc3ede8ddfe48d8ec7
Byron Hall <byronhall@MacBook-Pro.attlocal.net>
2021-01-23 14:25:07 -0600

    added jquery to capture column dimensions

--- src/components/subcomponents/CustomDialogModal.vue
+++ src/components/subcomponents/CustomDialogModal.vue
@@ -1,13 +1,39 @@
 <template>
   <v-row no-gutters style="height: 100%;">
       <v-col>Foo</v-col>
-      <v-col>Bar</v-col>
+      <v-col id="chess-board-column">
+          width: {{chessBoardColumnWidth}} <br />
+          height: {{chessBoardColumnHeight}} <br />
+      </v-col>
   </v-row>
 </template>
 
 <script>
-export default {
+import $ from 'jquery';
 
+export default {
+    name: 'CustomDialogModal',
+    data() {
+        return {
+            chessBoardColumnWidth: null,
+            chessBoardColumnHeight: null,
+        };
+    },
+    methods: {
+        updateChessBoardDimensions() {
+            this.chessBoardColumnWidth = $('#chess-board-column').width();
+            this.chessBoardColumnHeight = $('#chess-board-column').height();
+        },
+    },
+    mounted() {
+        this.$nextTick(()=>{
+            this.updateChessBoardDimensions();
+        });
+        window.addEventListener('resize', this.updateChessBoardDimensions);
+    },
+    beforeDestroy() {
+        window.removeEventListener('resize', this.updateChessBoardDimensions);
+    },
 }
 </script>
 
