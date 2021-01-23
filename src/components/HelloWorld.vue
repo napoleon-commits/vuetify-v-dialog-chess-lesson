ca539d899b369c8252d00b82e5e304b05ae14672
Byron Hall <byronhall@MacBook-Pro.attlocal.net>
2021-01-23 16:17:42 -0600

    added v-dialog component

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,28 @@
 <template>
   <v-container>
-    Foo Bar
+    <v-dialog>
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn
+          v-bind="attrs"
+          v-on="on"
+          block
+          outlined
+        >
+          <span>
+            The magic position
+          </span>
+        </v-btn>
+      </template>
+      <div
+        :style="`
+        padding: 16px;
+        height: 100%;
+        background-color: ${$vuetify.theme.dark?'rgba(0,0,0,0.87)':'white'}
+        `"
+      >
+
+      </div>
+    </v-dialog>
   </v-container>
 </template>
 
@@ -9,3 +31,9 @@
     name: 'HelloWorld',
   }
 </script>
+
+<style>
+  .v-dialog:not(.v-dialog--fullscreen){
+    height: 90%;
+  }
+</style>
