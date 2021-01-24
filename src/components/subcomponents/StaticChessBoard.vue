66c1f23b49eee908484b9d611b12f7a71751c654
Byron Hall <byronhall@MacBook-Pro.attlocal.net>
2021-01-23 20:14:21 -0600

    used vue-chessboard module

---
+++ src/components/subcomponents/StaticChessBoard.vue
@@ -0,0 +1,13 @@
+<script>
+  import { chessboard }  from 'vue-chessboard';
+
+  export default {
+    name: 'StaticChessBoard',
+    extends: chessboard,
+    mounted() {
+      this.board.set({
+        viewOnly: true
+      })
+    }
+  };
+</script>
